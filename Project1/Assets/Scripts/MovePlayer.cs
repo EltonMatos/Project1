451538b7cf00f7478522218d9a1a1be50b3ed195
EltonMatos <eltonnogueira@msn.com>
2021-03-01 19:51:41 -0300

    add script MovePlayer and Colliders

---
+++ Project1/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 30f;
+    public float turnSpeed = 90f;
+   
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {        
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.up, -turnSpeed * Time.deltaTime);
+        }
+    }
+}
